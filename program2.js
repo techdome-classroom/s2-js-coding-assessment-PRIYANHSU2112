1e89f789a2b88f69ffcb32783135ae0845b1dcd1
PRIYANSHU SAHU2112 <127828893+PRIYANHSU2112@users.noreply.github.com>
2024-09-30 14:15:55 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -12,29 +12,19 @@ var romanToInt = function(s) {
         'D': 500,
         'M': 1000
     };
-
     let total = 0;
     let prevValue = 0;
-
-    // Iterate through the string from right to left
     for (let i = s.length - 1; i >= 0; i--) {
         const currentChar = s[i];
         const currentValue = romanMap[currentChar];
-
-        // If the current value is less than the previous value, we subtract it
         if (currentValue < prevValue) {
             total -= currentValue;
         } else {
-            // Otherwise, we add it
             total += currentValue;
         }
 
-        // Update the previous value for the next iteration
         prevValue = currentValue;
     }
-
     return total;
 };
-
-
 module.exports={romanToInt}
